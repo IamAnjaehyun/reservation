2e47d6a423294d1bbb7e640e59cffd8a182d6627
Anjaehyun <lvet0330@gmail.com>
2023-06-23 17:27:16 +0900

    fix : extens RuntimeException
    런타임 예외 받아오도록 수정.
    추후 오류 관련 코드 전체 수정 예정

--- src/main/java/com/jaehyun/reservation/global/exception/DuplicatedIdOrPhoneNumException.java
+++ src/main/java/com/jaehyun/reservation/global/exception/DuplicatedIdOrPhoneNumException.java
@@ -4,7 +4,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(code = HttpStatus.CONFLICT)
-public class DuplicatedIdOrPhoneNumException extends Exception {
+public class DuplicatedIdOrPhoneNumException extends RuntimeException {
 
   static final String DUPLICATED_ID_PHONE_NUM = "중복되는 아이디 혹은 휴대폰 번호가 존재합니다.";
 
