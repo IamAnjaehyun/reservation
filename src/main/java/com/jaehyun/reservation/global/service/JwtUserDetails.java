27b86bf5b3f43b5ba86ef780a2aa45bc7dd56f21
Anjaehyun <lvet0330@gmail.com>
2023-06-25 03:47:56 +0900

    fix : jwt token & auth fix
    RoleType 을 Enum 으로 지정함으로 인해
    권한이 List 형식 일 때 정상적으로 돌아갔던 jwt 코드가 망가져서 전체 수정
    
    여러 stackflow, 블로그, gpt 를 뒤져보았지만 ROLE_ 은 spring security가 지정해준다는 말을 믿고 
    jwt 패키지 전체구조 변경해보고, 새로운 코드도 넣어보고 해봤지만 실패.
    기존 코드에서 user.getRoles() 와 user.getRoles().getKey() 해봤지만 안되던 것이 "ROLE_" 추가하니까 돌아갔음..

--- src/main/java/com/jaehyun/reservation/global/service/JwtUserDetails.java
+++ src/main/java/com/jaehyun/reservation/global/service/JwtUserDetails.java
@@ -1,36 +1,30 @@
 package com.jaehyun.reservation.global.service;
 
-import com.jaehyun.reservation.user.type.RoleType;
 import com.jaehyun.reservation.user.user.domain.entity.User;
 import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public class JwtUserDetails implements UserDetails {
 
-  private User user;
+  private final User user;
 
   public JwtUserDetails(User user) {
-    super();
     this.user = user;
   }
 
-
   @Override
+
   public Collection<? extends GrantedAuthority> getAuthorities() {
-    RoleType role = user.getRoles();
-    List<SimpleGrantedAuthority> authorities = new ArrayList<SimpleGrantedAuthority>();
 
-    authorities.add(new SimpleGrantedAuthority(role.toString()));
+    Collection<GrantedAuthority> authorities = new ArrayList<>();
+
+    authorities.add(new SimpleGrantedAuthority("ROLE_" + user.getRoles().getKey()));
+
     return authorities;
-  }
 
-  @Override
-  public String getPassword() {
-    return user.getPassword();
   }
 
   @Override
@@ -38,24 +32,31 @@ public class JwtUserDetails implements UserDetails {
     return user.getLoginId();
   }
 
+  @Override
+  public String getPassword() {
+    return user.getPassword();
+  }
+
+
+  // == 세부 설정 == //
 
   @Override
-  public boolean isAccountNonExpired() {
+  public boolean isAccountNonExpired() { // 계정의 만료 여부
     return true;
   }
 
   @Override
-  public boolean isAccountNonLocked() {
+  public boolean isAccountNonLocked() { // 계정의 잠김 여부
     return true;
   }
 
   @Override
-  public boolean isCredentialsNonExpired() {
+  public boolean isCredentialsNonExpired() { // 비밀번호 만료 여부
     return true;
   }
 
   @Override
-  public boolean isEnabled() {
+  public boolean isEnabled() { // 계정의 활성화 여부
     return true;
   }
 }
--- src/main/java/com/jaehyun/reservation/user/type/RoleType.java
+++ src/main/java/com/jaehyun/reservation/user/type/RoleType.java
@@ -1,6 +1,15 @@
 package com.jaehyun.reservation.user.type;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
 public enum RoleType {
-  USER,
-  ADMIN
+  USER("USER", "일반 유저"),
+  ADMIN("ADMIN", "상점 주인");
+
+  private final String key;
+  private final String title;
+
 }
