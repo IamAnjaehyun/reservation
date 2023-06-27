f99c5afe0646e3d76fdabfb3272558b3d3651b9c
Anjaehyun <lvet0330@gmail.com>
2023-06-27 17:41:40 +0900

    fix : errorFix and feat
    모든 customException AbstractException extends 하도록 수정
    고객이 이미 예약했던 시간에 또 예약하려고 한다면 예외처리를 통해 불가능하도록

--- src/main/java/com/jaehyun/reservation/user/reservation/domain/repository/ReservationRepository.java
+++ src/main/java/com/jaehyun/reservation/user/reservation/domain/repository/ReservationRepository.java
@@ -2,6 +2,7 @@ package com.jaehyun.reservation.user.reservation.domain.repository;
 
 import com.jaehyun.reservation.user.reservation.domain.entity.Reservation;
 import com.jaehyun.reservation.user.user.domain.entity.User;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ public interface ReservationRepository extends JpaRepository<Reservation, Long> 
 
   List<Reservation> findAllByUser(User user);
   Optional<Reservation> findByUserAndId(User user, Long id);
+
+  boolean existsByReservationDateTimeAndUser(LocalDateTime localDateTime, User user);
 }
