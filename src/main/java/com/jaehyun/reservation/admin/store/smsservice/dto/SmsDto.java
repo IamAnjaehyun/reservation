1b2c738a675b59c8c113ec6b74e316f67b270f95
Anjaehyun <lvet0330@gmail.com>
2023-07-07 20:58:38 +0900

    feat : sens setting
    sms: //환경변수 등록
      accessKey: ${sms_accessKey}
      secretKey: ${sms_secretKey}
      serviceId: ${sms_serviceId}
      senderPhone: ${sms_senderPhone}
    
    dto 작성

---
+++ src/main/java/com/jaehyun/reservation/admin/store/smsservice/dto/SmsDto.java
@@ -0,0 +1,17 @@
+package com.jaehyun.reservation.admin.store.smsservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
+public class SmsDto {
+  private String to;
+  private String content;
+}
