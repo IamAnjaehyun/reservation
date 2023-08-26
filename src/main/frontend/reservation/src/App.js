73f97a3273f18bb09da57a7d7e64c8a62ed8c7c0
Anjaehyun <lvet0330@gmail.com>
2023-08-26 18:24:06 +0900

    feat : change name using button
    버튼 클릭시 글 제목 바뀌는 기능 구현

--- src/main/frontend/reservation/src/App.js
+++ src/main/frontend/reservation/src/App.js
@@ -7,6 +7,12 @@ function App() {
 
   let [글제목 ,글제목변경] = useState(['요즘 뜨는 식당', '요즘 뜨는 식당 2', '요즘 뜨는 식당 3']); //변수 대신 쓰는 데이터 저장공간
   let [좋아요, 좋아요변경] = useState(0);
+
+  function 제목바꾸기(){
+    var newArray = [...글제목];
+    newArray[0] = '안뜨는 식당';
+    글제목변경(newArray);
+  }
   
   let styles = {color : 'gray', fontSize: '30px' }
   return (
@@ -15,6 +21,7 @@ function App() {
       {/* 변경 자주 안되니까 걍둠 */}
         <div style={ styles }>Reservation</div> 
       </div>
+      <button onClick={제목바꾸기}>버튼</button>
       <div className='list'>
         <h3> {글제목[0]} <span onClick={ ()=>{좋아요변경(좋아요+1)} }>👍</span> {좋아요} </h3>
         <p>레스토랑</p>
